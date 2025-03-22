c6f36e430709f7d78a46cfae9ef15b2257b7f980
Wellington Ramos <wellingtonjsramos@hotmail.com>
2025-03-22 20:43:28 -0300

    feat(meta): implemented more meta

--- app.vue
+++ app.vue
@@ -1,10 +1,3 @@
-<template>
-  <NuxtLayout>
-    <NuxtPage />
-    <NuxtRouteAnnouncer />
-  </NuxtLayout>
-</template>
-
 <script setup>
 import { useHead } from '#imports'
 
@@ -14,13 +7,23 @@ useHead({
     { name: 'description', content: 'Utilize nossa calculadora de eficiência energética para calcular o consumo do seu computador, obter dicas de economia de energia e contribuir para a sustentabilidade.' },
     { name: 'keywords', content: 'eficiência energética, consumo de energia, calculadora de energia, computador, PC, economia de energia, sustentabilidade' },
     { name: 'robots', content: 'index, follow' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     { property: 'og:title', content: 'Calculadora de Eficiência Energética - Calcule o Consumo de Energia do seu PC' },
     { property: 'og:description', content: 'Descubra como otimizar o consumo de energia do seu computador e economizar com dicas práticas para um uso mais sustentável.' },
     { property: 'og:type', content: 'website' },
-    { property: 'og:url', content: 'https://jsrwell.github.io/energy-efficiency-calculator/' }
+    { property: 'og:url', content: 'https://jsrwell.github.io/energy-efficiency-calculator/' },
+    { property: 'og:image', content: 'https://jsrwell.github.io/energy-efficiency-calculator/base.png' },
+    { property: 'og:image:width', content: '1823' },
+    { property: 'og:image:height', content: '819' },
+    { property: 'og:image:alt', content: 'Calculadora de Eficiência Energética' },
+    { property: 'og:site_name', content: 'Calculadora de Eficiência Energética' },
+    { property: 'og:locale', content: 'pt_BR' },
   ],
   link: [
-    { rel: 'icon', type: 'image/x-icon', href: '/energy-efficiency-calculator/favicon.ico' }
+    { rel: 'icon', type: 'image/x-icon', href: '/energy-efficiency-calculator/favicon.ico' },
+    { rel: 'canonical', href: 'https://jsrwell.github.io/energy-efficiency-calculator/' },
+    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+    { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
   ]
 })
 </script>
